f9391846820f487ed4bdc58eb8eb453f97cf1325
Antonio Manuel <anmamebo2001@gmail.com>
2025-01-30 16:55:01 +0100

    Backend - Refactor FastAPI initialization to use async lifespan context for database setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,18 @@
+from contextlib import asynccontextmanager
+
 from app.database.connection import create_db_and_tables
 from app.routes import auth
 from fastapi import FastAPI
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Inicialization of the database and tables."""
+    create_db_and_tables()  # Ejecuta antes de que la app arranque
+    yield  # Aquí FastAPI ejecuta la aplicación normalmente
 
 
-# TODO: Modificar el deprecated
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(auth.router)
