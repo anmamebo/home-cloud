d5e28f0d9a071ec643e501bfaa21e6d4136cd402
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-04 00:04:44 +0100

    Frontend - Refactor theme management: consolidate theme provider and context, update imports

---
+++ frontend/src/components/theme/index.ts
@@ -0,0 +1,4 @@
+export { useTheme } from "./hooks/use-theme";
+export { ModeToggle } from "./mode-toggle";
+export { ThemeProvider } from "./providers/theme-provider";
+export type { Theme } from "./types/theme-types";
