c22d88c096152797b7612ef7ac53bd88dad9e6f1
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-04 14:51:17 +0100

    Frontend - Delete redundant text

--- frontend/src/components/sidebar/nav-user.tsx
+++ frontend/src/components/sidebar/nav-user.tsx
@@ -115,7 +115,6 @@ export const NavUser = ({
                 <DropdownMenuSubTrigger>
                   <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                   <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Cambiar tema</span>
                   Cambiar tema
                 </DropdownMenuSubTrigger>
 
