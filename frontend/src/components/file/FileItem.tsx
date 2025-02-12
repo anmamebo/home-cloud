7a3c878234e6411e08441a786b6f6b63e6fc8e8b
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-12 13:44:26 +0100

    Frontend - Add Tooltip components to FileItem and FolderItem to improve accessibility

--- frontend/src/components/file/FileItem.tsx
+++ frontend/src/components/file/FileItem.tsx
@@ -1,6 +1,12 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { File } from "@/types";
 import { EllipsisVertical, FileIcon } from "lucide-react";
-import { Card, CardContent } from "../ui/card";
 
 type FileItemProps = {
   file: File;
@@ -8,22 +14,29 @@ type FileItemProps = {
 
 export const FileItem = ({ file }: FileItemProps) => {
   return (
-    <Card
-      className="p-4 cursor-pointer hover:bg-accent hover:text-accent-foreground transition-colors"
-      role="button"
-      tabIndex={0}
-    >
-      <CardContent className="flex items-center gap-4 p-0">
-        <div className="flex-none">
-          <FileIcon size={22} />
-        </div>
-        <div className="flex-grow truncate text-md font-medium">
-          {file.filename}
-        </div>
-        <div className="flex-none">
-          <EllipsisVertical size={18} />
-        </div>
-      </CardContent>
-    </Card>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Card
+            className="p-4 cursor-pointer hover:bg-accent hover:text-accent-foreground transition-colors"
+            role="button"
+            tabIndex={0}
+          >
+            <CardContent className="flex items-center gap-4 p-0">
+              <div className="flex-none">
+                <FileIcon size={22} />
+              </div>
+              <div className="flex-grow truncate text-md font-medium">
+                {file.filename}
+              </div>
+              <div className="flex-none">
+                <EllipsisVertical size={18} />
+              </div>
+            </CardContent>
+          </Card>
+        </TooltipTrigger>
+        <TooltipContent side="bottom">{file.filename}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
