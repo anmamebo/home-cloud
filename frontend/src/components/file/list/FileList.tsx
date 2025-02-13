cd91ac20c102d5e93e0349b19c6aa8ce980dcb21
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-13 19:50:25 +0100

    Frontend - Update grid layout in FileList and FolderList components for improved responsiveness

--- frontend/src/components/file/list/FileList.tsx
+++ frontend/src/components/file/list/FileList.tsx
@@ -7,7 +7,7 @@ type FileListProps = {
 
 export const FileList = ({ files }: FileListProps) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
       {files.map((item: File) => (
         <FileItem key={item.id} file={item} />
       ))}
