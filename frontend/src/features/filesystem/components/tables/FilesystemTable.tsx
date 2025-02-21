263c477f29270b3a25a3e1317fa4d19782bb3b8a
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-22 00:13:56 +0100

    Frontend - Add Table component and integrate it into FilesystemTable for improved data display

--- frontend/src/features/filesystem/components/tables/FilesystemTable.tsx
+++ frontend/src/features/filesystem/components/tables/FilesystemTable.tsx
@@ -1,4 +1,17 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { File, Folder } from "@/types";
+import { getFileIcon } from "@/utils/fileIconUtils";
+import { formatDateTime, formatFileSize } from "@/utils/formatUtils";
+import { FolderIcon } from "lucide-react";
 
 type FilesystemTableProps = {
   folders: Folder[];
@@ -8,5 +21,48 @@ type FilesystemTableProps = {
 export const FilesystemTable = ({ folders, files }: FilesystemTableProps) => {
   console.log(folders, files);
 
-  return <></>;
+  return (
+    <Table>
+      <TableCaption></TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Nombre</TableHead>
+          <TableHead>Tamaño</TableHead>
+          <TableHead>Fecha de creación</TableHead>
+          <TableHead>Acciones</TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {folders.map((folder) => (
+          <TableRow key={folder.id}>
+            <TableCell className="flex items-center gap-2">
+              <FolderIcon />
+              {folder.name}
+            </TableCell>
+            <TableCell>{"--"}</TableCell>
+            <TableCell>{formatDateTime(folder.created_at)}</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        ))}
+
+        {files.map((file) => {
+          const FileIcon = getFileIcon(file.filename);
+          return (
+            <TableRow key={file.id}>
+              <TableCell className="flex items-center gap-2">
+                <FileIcon />
+                {file.filename}
+              </TableCell>
+              <TableCell>{formatFileSize(file.filesize)}</TableCell>
+              <TableCell>{formatDateTime(file.created_at)}</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+
+      <TableFooter></TableFooter>
+    </Table>
+  );
 };
