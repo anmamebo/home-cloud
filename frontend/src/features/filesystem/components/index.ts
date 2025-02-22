83688f540bad5d55381e8f6aa5bd42291473d6b4
Antonio Manuel <anmamebo2001@gmail.com>
2025-02-22 14:25:01 +0100

    Frontend - Refactor filesystem component exports to streamline imports and improve organization

---
+++ frontend/src/features/filesystem/components/index.ts
@@ -0,0 +1,37 @@
+// dialogs
+export { ChangeNameDialog } from "./dialogs/ChangeNameDialog";
+export { CreateFolderDialog } from "./dialogs/CreateFolderDialog";
+export { UploadFileDialog } from "./dialogs/UploadFileDialog";
+
+// filters
+export { FileFilters } from "./filters/FileFilters";
+export { FolderFilters } from "./filters/FolderFilters";
+
+// forms
+export { ChangeNameFileForm } from "./forms/ChangeNameFileForm";
+export { ChangeNameFolderForm } from "./forms/ChangeNameFolderForm";
+export { ChangeNameForm } from "./forms/ChangeNameForm";
+export { CreateFolderForm } from "./forms/CreateFolderForm";
+export { UploadFileForm } from "./forms/UploadFileForm";
+
+// items
+export { FileItem } from "./items/FileItem";
+export { FolderItem } from "./items/FolderItem";
+
+// lists
+export { FileList } from "./lists/FileList";
+export { FolderList } from "./lists/FolderList";
+
+// menus
+export { FileContextMenuContent } from "./menus/FileContextMenuContent";
+export { FileDropdownMenuContent } from "./menus/FileDropdownMenuContent";
+export { FolderContextMenuContent } from "./menus/FolderContextMenuContent";
+export { FolderDropdownMenuContent } from "./menus/FolderDropdownMenuContent";
+export { ItemDropdownMenu } from "./menus/ItemDropdownMenu";
+
+// tables
+export { FilesystemTable } from "./tables/FilesystemTable";
+
+export { FilesystemHeader } from "./FilesystemHeader";
+export { FilesystemSection } from "./FilesystemSection";
+export { NoContent } from "./NoContent";
--- frontend/src/features/filesystem/index.ts
+++ frontend/src/features/filesystem/index.ts
@@ -1,41 +1,5 @@
 // components
-// dialogs
-export { ChangeNameDialog } from "./components/dialogs/ChangeNameDialog";
-export { CreateFolderDialog } from "./components/dialogs/CreateFolderDialog";
-export { UploadFileDialog } from "./components/dialogs/UploadFileDialog";
-
-// filters
-export { FileFilters } from "./components/filters/FileFilters";
-export { FolderFilters } from "./components/filters/FolderFilters";
-
-// forms
-export { ChangeNameFileForm } from "./components/forms/ChangeNameFileForm";
-export { ChangeNameFolderForm } from "./components/forms/ChangeNameFolderForm";
-export { ChangeNameForm } from "./components/forms/ChangeNameForm";
-export { CreateFolderForm } from "./components/forms/CreateFolderForm";
-export { UploadFileForm } from "./components/forms/UploadFileForm";
-
-// items
-export { FileItem } from "./components/items/FileItem";
-export { FolderItem } from "./components/items/FolderItem";
-
-// lists
-export { FileList } from "./components/lists/FileList";
-export { FolderList } from "./components/lists/FolderList";
-
-// menus
-export { FileContextMenuContent } from "./components/menus/FileContextMenuContent";
-export { FileDropdownMenuContent } from "./components/menus/FileDropdownMenuContent";
-export { FolderContextMenuContent } from "./components/menus/FolderContextMenuContent";
-export { FolderDropdownMenuContent } from "./components/menus/FolderDropdownMenuContent";
-export { ItemDropdownMenu } from "./components/menus/ItemDropdownMenu";
-
-// tables
-export { FilesystemTable } from "./components/tables/FilesystemTable";
-
-export { FilesystemHeader } from "./components/FilesystemHeader";
-export { FilesystemSection } from "./components/FilesystemSection";
-export { NoContent } from "./components/NoContent";
+export * from "./components";
 
 // constants
 export { columns } from "./constants/ColumnsConstants";
